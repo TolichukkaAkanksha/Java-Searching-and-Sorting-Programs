b1698e3825dcd7a51bd46b7bd1746bd93d6d8127
Tolichukka Akanksha <125852692+TolichukkaAkanksha@users.noreply.github.com>
2023-03-27 09:38:39 +0530

    Update LinearSearch.java

--- LinearSearch.java
+++ LinearSearch.java
@@ -21,12 +21,7 @@ public class Main
         }
         else{
             System.out.println("Element not found!");
-        }
-        
-        
-        
-	
-		
+	}		
 	}
 }
 class LinearSearch{
